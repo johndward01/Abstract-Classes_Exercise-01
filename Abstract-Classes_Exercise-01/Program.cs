1ebf677be7285a0a56294fba7043dd9eb2c1f2f7
johndward01 <johndward01@gmail.com>
2022-09-04 00:08:55 -0500

    updated TODO's

--- Abstract-Classes_Exercise-01/Program.cs
+++ Abstract-Classes_Exercise-01/Program.cs
@@ -1,10 +1,28 @@
 ﻿// TODO: Create an abstract class called Part 
-// TODO: Create 1 abstract field, 1 abstract property, and 1 abstract method
-// BONUS: Create 1 virtual method 
+// TODO:  Inside of the abstract Part class, Create 3 abstract properties called ID, Name, and Price 
+// TODO:  Inside of the abstract Part class, Create 1 abstract void method called PrintPartInfo() 
+// TODO:  Inside of the abstract Part class, Create 1 private static field of type ulong called _partCount;
+// TODO:  Inside of the abstract Part class, Create 1 parameterized constructor foreach of the 4 properties and also increment the _partCount field
+// BONUS: Inside of the abstract Part class, Create 1 static void method called PrintTotalPartCount() 
 
 // TODO: Create 3 concrete classes that inherit from the abstract Part class
 // TODO: Implement the abstract class and add any extra functionality to your concrete class
 
-// TODO: Instantiate all 3 classes and give their members values 
+// TODO: Instantiate all 3 concrete classes and give their members values
+
 // TODO: Creat a list of type Part and add the instances to the list
-// TODO: Using a loop, execute each part's method {Console.WriteLine() the result if the method isn't void}
+
+// TODO: Using a loop, execute each part's method Console.WriteLine() the result if the method isn't void
+
+AddSpaces(1);
+
+// Bonus: Call the PrintTotalPartCount() method
+
+void AddSpaces(int numberOfSpaces)
+{
+    while (numberOfSpaces > 0)
+    {
+        Console.WriteLine();
+        numberOfSpaces--;
+    }
+}
