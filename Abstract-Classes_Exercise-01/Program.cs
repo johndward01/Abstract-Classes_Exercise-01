2669dc803c479253ac861e1fd2ab4b36f2ad82db
johndward01 <johndward01@gmail.com>
2022-09-03 23:05:12 -0500

    updated TODO's

--- Abstract-Classes_Exercise-01/Program.cs
+++ Abstract-Classes_Exercise-01/Program.cs
@@ -1,8 +1,9 @@
-﻿// TODO: Create an abstract class called Part
+﻿// TODO: Create an abstract class called Part 
+// TODO: Create 1 abstract field, 1 abstract property, and 1 abstract method
+// BONUS: Create 1 virtual method 
 
 // TODO: Create 3 concrete classes that inherit from the abstract Part class
-
-// TODO: Create 1 field, 1 property, 1 parameterized constructor, and 1 method for each of the 3 concrete classes
+// TODO: Implement the abstract class and add any extra functionality to your concrete class
 
 // TODO: Instantiate all 3 classes and give their members values 
 // TODO: Creat a list of type Part and add the instances to the list
