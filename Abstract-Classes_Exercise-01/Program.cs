01e29ba09ac671b09f1b0909b0e7778b56462422
johndward01 <johndward01@gmail.com>
2022-09-03 23:00:47 -0500

    init

--- Abstract-Classes_Exercise-01/Program.cs
+++ Abstract-Classes_Exercise-01/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// TODO: Create an abstract class called Part
+
+// TODO: Create 3 concrete classes that inherit from the abstract Part class
+
+// TODO: Create 1 field, 1 property, 1 parameterized constructor, and 1 method for each of the 3 concrete classes
+
+// TODO: Instantiate all 3 classes and give their members values 
+// TODO: Creat a list of type Part and add the instances to the list
+// TODO: Using a loop, execute each part's method {Console.WriteLine() the result if the method isn't void}
